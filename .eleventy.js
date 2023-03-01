e2809ba7529db7f0cb7dee9b6c80636db24d19aa
Javier Bullrich <javier@bullrich.dev>
2023-03-01 17:50:44 +0100

    added html minification in production

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const eleventySass = require("eleventy-sass");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const htmlmin = require("html-minifier");
 
 module.exports = config => {
     // Filters
@@ -42,6 +43,21 @@ module.exports = config => {
     // Plugins
     config.addPlugin(eleventySass);
     config.addPlugin(syntaxHighlight);
+
+    if (process.env.NODE_ENV === "production") {
+        config.addTransform("htmlmin", function (content) {
+            // Prior to Eleventy 2.0: use this.outputPath instead
+            if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+                let minified = htmlmin.minify(content, {
+                    useShortDoctype: true,
+                    removeComments: true,
+                    collapseWhitespace: true
+                });
+                return minified;
+            }
+            return content;
+        });
+    }
 
     return {
         markdownTemplateEngine: 'njk',
